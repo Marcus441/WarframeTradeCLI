766eabaa9758d7bbd4fc6d927394773f5221cc2a
Marcus441 <psminchia4153@hotmail.com>
2025-07-11 21:17:56 +1000

    style(formatting)

--- src/core/market_api.cpp
+++ src/core/market_api.cpp
@@ -11,22 +11,16 @@ using json = nlohmann::json;
 
 constexpr const char *MARKET_URL = "https://api.warframe.market/v1";
 
-// TODO: change return type of these to json and separate console output logic
-
 json searchItem(std::string &query) {
-  // std::cout << "Searching for: " << query << '\n';
   cpr::Response r =
       cpr::Get(cpr::Url{std::string(MARKET_URL) + "/items/" + query});
-  // std::cout << "Status: " << r.status_code << '\n';
   json data = json::parse(r.text);
   return data;
 }
 
 json searchItemOrders(std::string &query) {
-  // std::cout << "Searching orders for: " << query << '\n';
   cpr::Response r = cpr::Get(
       cpr::Url{std::string(MARKET_URL) + "/items/" + query + "/orders"});
-  // std::cout << "Status: " << r.status_code << '\n';
   json data = json::parse(r.text);
   return data;
 }
