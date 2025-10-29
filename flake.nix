b92e5539f2ba7d3b6e57d9b11f30ed29d0c5bb19
Marcus441 <psminchia4153@hotmail.com>
2025-10-29 12:16:32 +1000

    chore(buildsystem): moved build system over to nix
    - Use find_package to source libraries
    - Use derivation based build system, run from result/ directory
    - Now uses cpr and nlohmann_json in devshell input

--- flake.nix
+++ flake.nix
@@ -17,21 +17,51 @@
 
         buildInputs = with pkgs; [
           cmake
-          gnumake
           bear
-          clang # Clang
-          clang-tools # Extra tools: clang-format, clang-tidy, etc.
-          lldb # LLVM debugger
-          gdb # GNU debugger for comparison
+          gdb
           meson
           ninja
           openssl
+          nlohmann_json
+          cpr
+          curl
         ];
 
         shellHook = ''
           echo "🛠️  C++ dev shell with Clang"
           echo "🔧  Compiler: $("${pkgs.clang}/bin/clang" --version | head -n 1)"
         '';
+      };
+      packages.wftcli = pkgs.stdenv.mkDerivation {
+        pname = "warframe-trade-cli";
+        version = "0.0.1";
+
+        src = ./.;
+
+        nativeBuildInputs = with pkgs; [
+          gcc
+          cmake
+          pkg-config
+        ];
+        buildInputs = with pkgs; [
+          cpr
+          curl
+          nlohmann_json
+        ];
+
+        cmakeFlags = [
+          "-DCMAKE_BUILD_TYPE=Release"
+          "-DCPR_USE_SYSTEM_CURL=ON"
+        ];
+
+        # buildPhase = ''
+        #   cmake -S . -B build $cmakeFlags
+        #   cmake --build build --parallel
+        # '';
+        #
+        # installPhase = ''
+        #   cmake --install build --prefix $out
+        # '';
       };
     });
 }
